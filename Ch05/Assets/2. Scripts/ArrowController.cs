c464373f6ac8a1c375689b7a745b1e7ff4df8aa3
tjdus7 <psyb062@gmail.com>
2026-04-01 15:44:20 +0900

    Ch05 시작 04/01

---
+++ Ch05/Assets/2. Scripts/ArrowController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    public float dropSpeed = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -dropSpeed, 0);
+
+        if(transform.position.y < -7)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
